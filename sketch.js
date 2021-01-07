86ed510e3d28d4a5d33801d05acf66db3868e75d
ammarshahin <ammar0ammar612@gmail.com>
2021-01-07 19:20:11 +0200

    dummy

--- sketch.js
+++ sketch.js
@@ -1,10 +1,17 @@
-function setup() {
-  createCanvas(1500, 700);
-  console.log("ammr");
+var font = [0, 0, 0];
+
+function setup()
+{
+    createCanvas(1500, 700);
+    console.log("Ammar Shahin");
+    font[0] = 100;
+    font[1] = 100;
+    font[2] = 100;
 }
 
-function draw() {
-  background(255);
-  circle(200, 200, 200);
-  fill(204, 102, 0);
+function draw()
+{
+    background(255);
+    circle(200, 200, 200);
+    fill(font[0], font[1], font[2]);
 }
