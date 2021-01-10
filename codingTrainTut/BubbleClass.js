97dfe5acb5a4084dc5c322aa19e8b2cd1a076d68
AmmarMShahin <Ammar-Shahin@elarabygroup.com>
2021-01-10 14:44:56 +0200

    adding some stuff

--- codingTrainTut/BubbleClass.js
+++ codingTrainTut/BubbleClass.js
@@ -1,83 +1,76 @@
-class Bubble
-{
-    constructor(x, y, size, speed, color)
-    {
-        this.x = x;
-        this.y = y;
-        this.speed = speed;
-        this.size = size;
-        this.color = color;
+class Bubble {
+  constructor(x, y, size, speed, color) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.size = size;
+    this.color = color;
+  }
+
+  move() {
+    this.x += this.speed[0];
+    this.y += this.speed[1];
+    if (this.x > width) {
+      this.x = 0;
     }
 
-    move()
-    {
-        this.x += this.speed[0];
-        this.y += this.speed[1];
-        if (this.x > width)
-        {
-            this.x = 0;
-        }
-
-        if (this.y > hight)
-        {
-            this.y = 0;
-        }
+    if (this.y > hight) {
+      this.y = 0;
     }
+  }
 
-    show()
-    {
-        noStroke();
-        fill(this.color);
-        circle(this.x, this.y, this.size);
-    }
+  show() {
+    noStroke();
+    fill(this.color);
+    circle(this.x, this.y, this.size);
+  }
 
-    x_position_set(x)
-    {
-        this.x = x;
+  contains(px, py) {
+    let d = dist(px, py, this.x, this.y);
+    if (d < 0.5 * this.size) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    x_position_get()
-    {
-        return x;
-    }
+  x_position_set(x) {
+    this.x = x;
+  }
 
-    y_position_set(y)
-    {
-        this.y = y;
-    }
+  x_position_get() {
+    return this.x;
+  }
 
-    y_position_get()
-    {
-        return y;
-    }
+  y_position_set(y) {
+    this.y = y;
+  }
 
-    speed_set(speed)
-    {
-        this.speed = speed;
-    }
+  y_position_get() {
+    return this.y;
+  }
 
-    speed_get()
-    {
-        return this.speed;
-    }
+  speed_set(speed) {
+    this.speed = speed;
+  }
 
-    size_set(size)
-    {
-        this.size = size;
-    }
+  speed_get() {
+    return this.speed;
+  }
 
-    size_get()
-    {
-        return this.size;
-    }
+  size_set(size) {
+    this.size = size;
+  }
 
-    color_set(color)
-    {
-        this.color = color;
-    }
+  size_get() {
+    return this.size;
+  }
 
-    color_get()
-    {
-        return this.color;
-    }
+  color_set(color) {
+    this.color = color;
+  }
+
+  color_get() {
+    return this.color;
+  }
 }
--- codingTrainTut/classesAndObjects.js
+++ codingTrainTut/classesAndObjects.js
@@ -1,12 +1,12 @@
 /**
  * @file classesAndObjects.js
  * @author Ammar Shahin (Ammar0Shahin@gmail.com)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2021-01-08
- * 
+ *
  * @copyright Copyright (c) 2021
- * 
+ *
  */
 
 const width = 600;
@@ -14,31 +14,86 @@ const hight = 600;
 
 let bubble;
 let array = [];
-const arraySize = 200;
+let arraySize = 40;
 
-function setup()
-{
-    createCanvas(width, hight);
-    for (let index = 0; index < arraySize; index++)
-    {
-        array[index] = new Bubble(0, random(600), random(10, 50), [random(5, 10), 0], random(50, 255));
-    }
+let count = 0;
+
+function setup() {
+  createCanvas(width, hight);
+  for (let index = 0; index < arraySize; index++) {
+    array[index] = new Bubble(
+      random(10, 600), // xPos
+      random(10, 600), // yPos
+      random(10, 20), // size
+      [0, 0], // speed
+      random(50, 255) // color
+    );
+  }
+
+  noOverLap();
+  console.log(count);
 }
 
-function draw()
-{
-    background(255);
-    for (let index = 0; index < arraySize; index++)
-    {
-        array[index].move();
-        array[index].show();
+function draw() {
+  background(255);
+  for (let index = 0; index < arraySize; index++) {
+    array[index].move();
+    array[index].show();
+  }
+
+  for (let i = 0; i < arraySize; i++) {
+    for (let j = 0; j < arraySize; j++) {
+      let xPos = array[j].x_position_get();
+      let yPos = array[j].y_position_get();
+      if (array[i].contains(xPos, yPos) && i != j) {
+        console.log(i, j);
+      }
     }
+  }
 }
 
-function mousePressed()
-{
-    for (let index = 0; index < arraySize; index++)
-    {
-        array[index].color_set(random(20, 255));
+function mousePressed() {
+  for (let index = 0; index < arraySize; index++) {
+    if (array[index].contains(mouseX, mouseY)) {
+      array.splice(index, 1);
+      index--;
+      let temp = new Bubble(
+        random(10, 600), // xPos
+        random(10, 600), // yPos
+        random(10, 20), // size
+        [0, 0], // speed
+        random(0, 255)
+      ); // color
+      array.push(temp);
     }
+  }
+}
+
+function noOverLap() {
+  /* no overlap between circles */
+  for (let i = 0; i < arraySize - 1; i++) {
+    for (let j = i + 1; j < arraySize; j++) {
+      let xPos = array[j].x_position_get();
+      let yPos = array[j].y_position_get();
+      if (array[i].contains(xPos, yPos)) {
+        console.log(i, j);
+        console.log(count);
+        i = 0;
+        j = 0;
+        array.splice(j, 1);
+        let temp = new Bubble(
+          random(10, 600), // xPos
+          random(10, 600), // yPos
+          random(10, 20), // size
+          [0, 0], // speed
+          random(0, 255)
+        ); // color
+        array.push(temp);
+        count++;
+        if (count >= 1000) {
+          return 0;
+        }
+      }
+    }
+  }
 }
